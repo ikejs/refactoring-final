63b89f142b3acf8a59dbaf73c892ad06254dd3bf
Ike <ikescode@gmail.com>
2021-03-04 14:58:05 -0600

    finish

--- tic_toc_toe_messy.py
+++ tic_toc_toe_messy.py
@@ -3,6 +3,8 @@
 
 import random
 
+magicNumber=10
+
 """
 This function prints out the board that it was passed.
 """
@@ -26,7 +28,6 @@ def inputPlayerLetter():
         print("Do you want to be X or O?")
         letter = input().upper()
 
-    # the first element in the list is the player’s letter, the second is the computer's letter.
     if letter == "X":
         return ["X", "O"]
     else:
@@ -37,16 +38,19 @@ Randomly choose the player who goes first.
 """
 def whoGoesFirst():
     if random.randint(0, 1) == 0:
+        print(f"The computer will go first.")
         return "computer"
     else:
+        print(f"You will go first.")
         return "player"
 
 """
 This function returns True if the player wants to play again, otherwise it returns False.
 """
 def playAgain():
-    print("Do you want to play again? (yes or no)")
-    return input().lower().startswith("y")
+    print("Do you want to play again? (Y/N)")
+    if input().lower().startswith("y"):
+        startGame()
 
 
 def makeMove(board, letter, move):
@@ -76,7 +80,7 @@ Make a duplicate of the board list and return it the duplicate.
 def getBoardCopy(board):
     dupeBoard = []
 
-    for i in range(0, len(board)):  # TODO: Clean this mess!
+    for i in range(0, len(board)):
         dupeBoard.append(board[i])
 
     return dupeBoard
@@ -94,7 +98,7 @@ Let the player type in their move.
 """
 def getPlayerMove(board):
     move = None
-    while move not in "1 2 3 4 5 6 7 8 9".split() or not isSpaceFree(board, int(move)):
+    while move not in "1 2 3 4 5 6 7 8 9".split()or not isSpaceFree(board, int(move)):
         print("What is your next move? (1-9)")
         move = input()
     return int(move)
@@ -119,33 +123,38 @@ def chooseRandomMoveFromList(board, movesList):
 Given a board and the computer's letter, 
 determine where to move and return that move.
 """
-# TODO: W0621: Redefining name 'computerLetter' from outer scope. Hint: Fix it according to https://stackoverflow.com/a/25000042/81306
 def getComputerMove(board, computerLetter):
     if computerLetter == "X":
         playerLetter = "O"
     else:
         playerLetter = "X"
 
-    # Here is our algorithm for our Tic Tac Toe AI:
-    # First, check if we can win in the next move
-    for i in range(1, 10):
+    """
+    Here is our algorithm for our Tic Tac Toe AI:
+    First, check if we can win in the next move
+    """
+    for i in range(1, magicNumber):
         copy = getBoardCopy(board)
         if isSpaceFree(copy, i):
             makeMove(copy, computerLetter, i)
             if isWinner(copy, computerLetter):
                 return i
 
-    # Check if the player could win on their next move, and block them.
-    for i in range(1, 10):
+    """
+    Check if the player could win on their next move, and block them.
+    """
+    for i in range(1, magicNumber):
         copy = getBoardCopy(board)
         if isSpaceFree(copy, i):
             makeMove(copy, playerLetter, i)
             if isWinner(copy, playerLetter):
                 return i
 
-    # Try to take one of the corners, if they are free.
+    """
+    Try to take one of the corners, if they are free.
+    """
     move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
-    if move is not None:# TODO: Fix it (Hint: Comparisons to singletons like None should always be done with is or is not, never the equality/inequality operators.)
+    if move is not None:
         return move
 
     # Try to take the center, if it is free.
@@ -158,59 +167,46 @@ def getComputerMove(board, computerLetter):
 
 def isBoardFull(board):
     # Return True if every space on the board has been taken. Otherwise return False.
-    for i in range(1, 10):
+    for i in range(1, magicNumber):
         if isSpaceFree(board, i):
             return False
     return True
 
+def checkGameOver(theBoard, playerLetter, computerLetter, turn):
+    if turn == "player":
+        if isWinner(theBoard, playerLetter):
+            print("Hooray! You have won the game!")
+        if isBoardFull(theBoard):
+            print("The game is a tie!")
+    else:
+        if isWinner(theBoard, computerLetter):
+            print("The computer has beaten you! You lose.")
+        if isBoardFull(theBoard):
+            print("The game is a tie!")
 
-print("Welcome to Tic Tac Toe!")
-
-# TODO: The following mega code block is a huge hairy monster. Break it down
-# into smaller methods. Use TODO s and the comment above each section as a guide
-# for refactoring.
-
-# Reset the board
-theBoard = [" "] * 10  # TODO: Refactor the magic number in this line (and all of the occurrences of 10 thare are conceptually the same.)
-playerLetter, computerLetter = inputPlayerLetter()
-turn = whoGoesFirst()
-print(f"The {turn} will go first.")
-gameIsPlaying = True  # TODO: Study how this variable is used. Does it ring a bell? (which refactoring method?)
-#       See whether you can get rid of this 'flag' variable. If so, remove it.
-
-while gameIsPlaying:  # TODO: Usually (not always), loops (or their content) are good candidates to be extracted into their own function.
-    #       Use a meaningful name for the function you choose.
+def play(theBoard, playerLetter, computerLetter, turn):
+    drawBoard(theBoard)
     if turn == "player":
         # Player’s turn.
-        drawBoard(theBoard)
         move = getPlayerMove(theBoard)
         makeMove(theBoard, playerLetter, move)
-
-        if isWinner(theBoard, playerLetter):
-            drawBoard(theBoard)
-            print("Hooray! You have won the game!")
-            gameIsPlaying = False
-        if isBoardFull(theBoard):
-            drawBoard(theBoard)
-            print("The game is a tie!")
-            break
-        turn = "computer"
-    
-        
-
+        checkGameOver(theBoard, playerLetter, computerLetter, "player")
+        play(theBoard, playerLetter, computerLetter, "computer")
     else:
         # Computer’s turn.
         move = getComputerMove(theBoard, computerLetter)
         makeMove(theBoard, computerLetter, move)
+        checkGameOver(theBoard, playerLetter, computerLetter, "computer")
+        play(theBoard, playerLetter, computerLetter, "player")
 
-        if isWinner(theBoard, computerLetter):
-            drawBoard(theBoard)
-            print("The computer has beaten you! You lose.")
-            gameIsPlaying = False
-        else:  # TODO: is this 'else' necessary?
-            if isBoardFull(theBoard):
-                drawBoard(theBoard)
-                print("The game is a tie!")
-                break
-            else:  # TODO: Is this 'else' necessary?
-                turn = "player"
+def startGame():
+    # Reset the board
+    theBoard = [" "] * magicNumber
+    playerLetter, computerLetter = inputPlayerLetter()
+    turn = whoGoesFirst()
+
+    play(theBoard, playerLetter, computerLetter, turn)
+
+
+print("Welcome to Tic Tac Toe!")
+startGame()
